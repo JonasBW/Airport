f00779b2f34473d756862674fed7bd4589a6676d
Yonas <yoni11er@gmail.com>
2021-09-22 09:45:52 +0200

    updates on methods

--- src/main/java/airport/Application.java
+++ src/main/java/airport/Application.java
@@ -23,9 +23,15 @@ public class Application {
         allAirplanes.add(new Peopleplane("B767",false,800,500,300));
         allAirplanes.add(new Peopleplane("B768",true,800, 500,400));
         allAirplanes.add(new Peopleplane("B769",true,800,500,500));
-
         allAirplanes.add(new Cargoplane("C100",true,800,2000,1100));
         allAirplanes.add(new Cargoplane("C101",false,800,4000,1500));
+
+        peoplePlanes.add(new Peopleplane("B767",false,800,500,300));
+        peoplePlanes.add(new Peopleplane("B768",true,800, 500,400));
+        peoplePlanes.add(new Peopleplane("B769",true,800,500,500));
+
+        cargoPlanes.add(new Cargoplane("C100",true,800,2000,1100));
+        cargoPlanes.add(new Cargoplane("C101",false,800,4000,1500));
 
         airports.add(new Airport("Eindhoven", allAirplanes));
 
@@ -41,10 +47,42 @@ public class Application {
 //        UnloadPassengers(planeId, unloadPassengers);
 //        Land(planeId);
 //        for(Peopleplane peoplePlane: peoplePlanes) System.out.println("Plane Id: " + peoplePlane.getPlaneId() + " Flying: " + peoplePlane.isCurrentlyFlying() + " Current no of passengers: " + peoplePlane.getCurrentNoOfPassengers());
-//          planesInAirport("Eindhoven");
-        landedPlanesWithSeatsInAirport("Eindhoven");
+//        LoadCargo("C100", 2350);
+//        UnloadCargo("C101");
+        landedPlanesWithAvailableSpaceInAirport("Eindhoven");
+          planesInAirport("Eindhoven");
     }
 
+
+    //Loading with cargo with a weight in tons
+    public static void LoadCargo(String planeId, int cargoInTons){
+        for(Cargoplane cargoPlane: cargoPlanes){
+            if (planeId.equals(cargoPlane.getPlaneId())){
+                int availableCargoSpace = cargoPlane.getMaxCargoInTons() - cargoPlane.getCurrentCargoInTons();
+                int excessCargo = cargoInTons - availableCargoSpace;
+                if(excessCargo > 0){
+                    System.out.println("You can not load more than the max " + cargoPlane.getMaxCargoInTons() + " cargo.");
+                    System.out.println("There is room for " + availableCargoSpace + ". But " + excessCargo + " do not fit");
+                }else{
+                    cargoPlane.setCurrentlyFlying(false);
+                    System.out.println("Plane " + planeId + " has loaded " + cargoInTons + " new passengers");
+                }
+            }
+        }
+    }
+
+    //Discharge of all cargo.
+    public static void UnloadCargo(String planeId){
+        for(Cargoplane cargoPlane: cargoPlanes){
+            if (planeId.equals(cargoPlane.getPlaneId())){
+                cargoPlane.setCurrentCargoInTons(0);
+                System.out.println("Plane " + planeId + " has discharged all cargo");
+            }
+        }
+    }
+
+    //Passenger planes
+    //Load a no of passengers in peoples plane
     public static void LoadPassengers(String planeId, int passengers){
         for(Peopleplane peoplePlane: peoplePlanes){
             if (planeId.equals(peoplePlane.getPlaneId())){
@@ -61,19 +99,7 @@ public class Application {
         }
     }
 
-    public static void TakeOff(String planeId){
-        for(Peopleplane peoplePlane: peoplePlanes){
-            if (planeId.equals(peoplePlane.getPlaneId())){
-                if(peoplePlane.isCurrentlyFlying()){
-                    System.out.println("This plane is already flying.");
-                }else{
-                    peoplePlane.setCurrentlyFlying(true);
-                    System.out.println("Plane " + planeId + " has taken off ");
-                }
-            }
-        }
-    }
-
+    //Unload a no of passengers from a people plane
     public static void UnloadPassengers(String planeId, int passengers){
         for(Peopleplane peoplePlane: peoplePlanes){
             if (planeId.equals(peoplePlane.getPlaneId())){
@@ -83,6 +109,20 @@ public class Application {
                     int remainingPassengers = peoplePlane.getCurrentNoOfPassengers() - passengers;
                     peoplePlane.setCurrentNoOfPassengers(remainingPassengers);
                     System.out.println("Plane " + planeId + " has unloaded " + passengers + " passengers");
+                }
+            }
+        }
+    }
+
+
+    public static void TakeOff(String planeId){
+        for(Peopleplane peoplePlane: peoplePlanes){
+            if (planeId.equals(peoplePlane.getPlaneId())){
+                if(peoplePlane.isCurrentlyFlying()){
+                    System.out.println("This plane is already flying.");
+                }else{
+                    peoplePlane.setCurrentlyFlying(true);
+                    System.out.println("Plane " + planeId + " has taken off ");
                 }
             }
         }
@@ -103,19 +143,21 @@ public class Application {
     public static void planesInAirport(String airportName){
         for(Airport airport: airports){
             if (airportName.equals(airport.getAirportName())){
-                List <Peopleplane> peoplePlanesInAirport = airport.getAirplanes();
+                List <Airplane> peoplePlanesInAirport = airport.getAirplanes();
                 System.out.println("People planes from airport: " + airportName);
-                for(Peopleplane peoplePlane: peoplePlanesInAirport) {
+                for(Airplane peoplePlane: peoplePlanesInAirport) {
+                    if(peoplePlane instanceof Peopleplane)
                     System.out.println("Plane Id: " + peoplePlane.getPlaneId() +
                             " Flying: " + peoplePlane.isCurrentlyFlying() +
-                            " Current no of passengers: " + peoplePlane.getCurrentNoOfPassengers());
+                            " Current no of passengers: " + ((Peopleplane) peoplePlane).getCurrentNoOfPassengers());
                 }
-                List <Cargoplane> cargoPlanesInAirport = airport.getAirplanes();
+                List <Airplane> cargoPlanesInAirport = airport.getAirplanes();
                 System.out.println("Cargo planes from airport: " + airportName);
-                for(Cargoplane cargoPlane: cargoPlanesInAirport) {
+                for(Airplane cargoPlane: cargoPlanesInAirport) {
+                    if(cargoPlane instanceof Cargoplane)
                     System.out.println("Plane Id: " + cargoPlane.getPlaneId() +
                             " Flying: " + cargoPlane.isCurrentlyFlying() +
-                            " Current cargo in tons: " + cargoPlane.getCurrentCargoInTons());
+                            " Current cargo in tons: " + ((Cargoplane) cargoPlane).getCurrentCargoInTons());
                 }
             }else{
                 System.out.println("Airport not found.");
@@ -123,7 +165,7 @@ public class Application {
         }
     }
 
-    public static void landedPlanesWithSeatsInAirport(String airportName){
+    public static void landedPlanesWithAvailableSpaceInAirport(String airportName){
         for(Airport airport: airports){
             if (airportName.equals(airport.getAirportName())){
                 List <Airplane> planesInAirport = airport.getAirplanes();
@@ -132,7 +174,7 @@ public class Application {
                         int availableSeat = ((Peopleplane) peoplePlane).getMaxNoOfPassengers() - ((Peopleplane) peoplePlane).getCurrentNoOfPassengers();
 
                         if(!peoplePlane.isCurrentlyFlying() && availableSeat > 0){
-                            System.out.println("Planes not flying with room for passengers: ");
+                            System.out.println("Planes not flying with room for passengers in airport " + airportName);
                             System.out.println("Plane Id: " + peoplePlane.getPlaneId() +
                                     " Available seats: " + availableSeat);
                         }
@@ -144,7 +186,7 @@ public class Application {
                     if(cargoPlane instanceof Cargoplane) {
                         int availableCargo = ((Cargoplane) cargoPlane).getMaxCargoInTons() - ((Cargoplane) cargoPlane).getCurrentCargoInTons();
                         if (!cargoPlane.isCurrentlyFlying() && availableCargo > 0) {
-                            System.out.println("Cargo planes not flying and with room for cargo: " + airportName);
+                            System.out.println("Cargo planes not flying and with room for cargo in airport " + airportName);
                             System.out.println("Plane Id: " + cargoPlane.getPlaneId() +
                                     " Flying: " + cargoPlane.isCurrentlyFlying() +
                                     " Current cargo in tons: " + ((Cargoplane) cargoPlane).getCurrentCargoInTons());
